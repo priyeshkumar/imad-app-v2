885db2fd732690a3cddcb90d3841f8d65b34ee7b
priyeshkumar <priyeshkumar@users.noreply.github.com>
2017-02-14 03:10:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne= {
+var articles= {
+ 'article-one': {
     title: 'Article One | Priyesh kumar',
     heading: 'Article One',
     date: 'feb 14, 2017',
@@ -18,7 +19,27 @@ var articleOne= {
                 <p>
                 This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
                 </p>`
+},
+  'article-two': { 
+    title: 'Article Two | Priyesh kumar',
+    heading: 'Article Two',
+    date: 'feb 14, 2017',
+    content:`<p>
+                This is the content for my second article.
+             </p>`
+      
+  },
+  'article-three': {
+    title: 'Article Three | Priyesh kumar',
+    heading: 'Article Three',
+    date: 'feb 14, 2017',
+    content:`<p>
+                This is the content for my third article.
+              </p>`
+      
+  }
 };
+
 function createTemplate(data){
  var title= data.title;
  var date= data.date;
@@ -55,20 +76,16 @@ function createTemplate(data){
 `;
 return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+    //articleName==article-one
+    //articles[articleName]=={} content object for article one
+    var articleName= req.param.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
