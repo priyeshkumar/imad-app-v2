9c1d5892477b13903db8b602be232eb2e547cbf0
priyeshkumar <priyeshkumar@users.noreply.github.com>
2017-03-07 10:12:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'priyeshkumar',
+    database : 'priyeshkumar',
+    host : 'db.imad.hasura-app.io',
+    password : process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,6 +87,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function (req, res){
+   //make a select request
+   //return a responce with the result
+   pool.query(' SELECT * test', function(err , result){
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   });
 });
 
 app.get('/:articleName', function (req, res) {
