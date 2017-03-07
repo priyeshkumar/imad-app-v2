7f87fbe1afa021540f593914c979c00199a18a83
priyeshkumar <priyeshkumar@users.noreply.github.com>
2017-03-07 10:33:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database : 'priyeshkumar',
     host : 'db.imad.hasura-app.io',
     port: '5432',
-    password : process.env.db-priyeshkumar-20836
+    password : 'process.env.db-priyeshkumar-20836'
 };
 
 var app = express();
